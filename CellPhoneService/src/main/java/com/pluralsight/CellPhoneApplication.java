cbf999748f0293684e9c7249b36101b469c440f3
jenidub <stanfordjeni@gmail.com>
2025-04-21 14:33:54 -0700

    Complete CellPhoneApplication from Section 2-1

---
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,84 @@
+package com.pluralsight;
+import java.util.*;
+
+public class CellPhoneApplication {
+    public static class CellPhone {
+        // Define fields
+        public String serialNumber, model, carrier, phoneNumber, owner;
+
+        // Class definition - parameterless w/ default values
+        public CellPhone() {
+            this.serialNumber = "0";
+            this.model = "";
+            this.carrier = "";
+            this.phoneNumber = "";
+            this.owner = "";
+        }
+
+        public String getSerialNumber() {
+            return this.serialNumber;
+        }
+
+        public void setSerialNumber(String serialNumber) {
+            this.serialNumber = serialNumber;
+        }
+
+        public String getModel() {
+            return this.model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public String getCarrier() {
+            return this.carrier;
+        }
+
+        public void setCarrier(String carrier) {
+            this.carrier = carrier;
+        }
+
+        public String getPhoneNumber() {
+            return this.phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getOwner() {
+            return this.owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Init scanner to take in user input
+        Scanner user_input = new Scanner(System.in);
+
+        // Init new cellPhone account
+         CellPhone account_one = new CellPhone();
+
+        // Prompt user for the required info
+        System.out.println("What is the serial number of your cell phone?   ");
+        account_one.serialNumber = user_input.nextLine();
+
+        System.out.println("What is the model of the phone?   ");
+        account_one.model = user_input.nextLine();
+
+        System.out.println("Who is the phone carrier?   ");
+        account_one.carrier = user_input.nextLine();
+
+        System.out.println("What is the phone number [###-###-####] ?   ");
+        account_one.phoneNumber = user_input.nextLine();
+
+        System.out.println("Who is the owner of the phone?   ");
+        account_one.owner = user_input.nextLine();
+
+        System.out.println(account_one.toString());
+    }
+}
---
+++ src/main/java/com/pluralsite/CellPhoneApplication.java
@@ -0,0 +1,7 @@
+public class CellPhone {
+    //Instance variables (fields)
+    private String serialNumber, model, carrier, phoneNumber, owner;
+
+    //Class definition
+
+}
